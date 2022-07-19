725c396fb33bf50f718da8d62300ef66f2936d80
fqaiser94 <fqaiser94@gmail.com>
2022-07-18 23:01:32 -0400

    randomize

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,13 +3,18 @@ import com.raquo.airstream.timing.PeriodicEventStream
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
 
+import scala.util.Random
+
 object Main {
   def main(args: Array[String]): Unit = {
     val svgSize = 500
     val squareSize = 50
     val color = Color.black
     val range = 1 to svgSize by squareSize
-    val coords = range.flatMap(x => range.map(y => Coord(x, y))).toList
+    val coords = range
+      .flatMap(x => range.map(y => Coord(x, y)))
+      .toList
+      .sortBy(_ => Random.nextInt(10))
     val squares = coords.map(coord => Square(squareSize, coord, color))
     val intervalMs = 10
     val squaresStream = new PeriodicEventStream[List[Square]](
